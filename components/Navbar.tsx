a8f532f3bc6b88ebbfb2e685076c3dde34311323
maxgonick <maxwellgonick@g.ucla.edu>
2023-03-05 15:54:07 -0800

    Initial Components

---
+++ components/Navbar.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import styles from "../styles/Navbar.module.css";
+import Link from 'next/link'
+import Image from 'next/image'
+
+type Props = {};
+
+const Navbar = (props: Props) => {
+  return (
+    <div>
+      <nav className={styles.navbar}>
+        <Link href='/'>About Us</Link>
+          <Link href='/'>Beanie Tutorial</Link>
+          <div>Forms</div>
+          <button><Link href='/'>Contact Us</Link></button>
+      </nav>
+    </div>
+  );
+};
+
+export default Navbar;
