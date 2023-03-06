7d05ea6c2c62d3fe34fd95e8b67ee83ffedf1ee6
maxgonick <maxwellgonick@g.ucla.edu>
2023-03-06 01:31:44 -0800

    Created Navbar basic design with minimal non-responsive CSS styling

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -3,20 +3,34 @@ import styles from "../styles/Navbar.module.css";
 import Link from "next/link";
 import Image from "next/image";
 import whiteLogo from "@/public/white-logo.png";
+import { Lexend } from "next/font/google";
+
+const lexend = Lexend({
+  subsets: ["latin"],
+  weight: "400",
+});
 
 type Props = {};
 
+// TODO responsive design!
+
 const Navbar = (props: Props) => {
   return (
-    <div>
+    <div className={lexend.className}>
       <nav className={styles.navbar}>
-        <Image src={whiteLogo} alt="logo" />
-        <Link href="/">About Us</Link>
-        <Link href="/">Beanie Tutorial</Link>
-        <div>Forms</div>
-        <button>
-          <Link href="/">Join Us</Link>
-        </button>
+        <Image src={whiteLogo} alt="logo" className={styles.logo} />
+        <div className={styles.right}>
+          <Link href="/" className={styles.links}>
+            About Us
+          </Link>
+          <Link href="/" className={styles.links}>
+            Beanie Tutorial
+          </Link>
+          <div className={styles.links}>Forms</div>
+          <Link href="/" className={styles.links}>
+            <button className={styles.button}>Join Us!</button>
+          </Link>
+        </div>
       </nav>
     </div>
   );
