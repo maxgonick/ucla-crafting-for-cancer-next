4444c94be990f8e7a59a8a968afc9e732c7631dd
maxgonick <maxwellgonick@g.ucla.edu>
2023-03-25 13:00:58 -0700

    Refactoring and fixing links

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -4,24 +4,52 @@ import { MdEmail } from "react-icons/md";
 import Image from "next/image";
 import blackLogo from "@/public/black-logo.png";
 import styles from "../styles/Footer.module.css";
+import { group } from "console";
+import Link from "next/link";
 
 interface Props {
   className?: string;
 }
 
 const Footer: React.FC<Props> = (props: Props) => {
+  const groupMeLink = (): void => {
+    window.open("https://www.google.com");
+  };
+  const instaLink = (): void => {
+    window.open("https://www.instagram.com");
+  };
+  const emailLink = (): void => {
+    window.location.assign("mailto:maxgonick@gmail.com");
+  };
+
   return (
     <div className={props.className}>
       <div className={styles.icons}>
-        <Image src={blackLogo} alt="logo" height={65} />
+        <Link href="/">
+          <Image src={blackLogo} alt="logo" height={65} />
+        </Link>
         <div className={styles.socials}>
-          <SiGroupme size={35} />
-          <SiInstagram size={35} />
-          <MdEmail size={35} />
+          <SiGroupme
+            size={35}
+            role="button"
+            tabIndex={0}
+            onClick={groupMeLink}
+          />
+          <SiInstagram
+            size={35}
+            role="button"
+            tabIndex={0}
+            onClick={instaLink}
+          />
+          <MdEmail size={35} role="button" tabIndex={0} onClick={emailLink} />
         </div>
       </div>
       <div className={styles.credit}>
-        Made with ❤️ by Maxwell Gonick with Next.js
+        Made with ❤️ by{" "}
+        <a href="https://github.com/maxgonick" className={styles.reset}>
+          Maxwell Gonick
+        </a>{" "}
+        with Next.js
       </div>
     </div>
   );
